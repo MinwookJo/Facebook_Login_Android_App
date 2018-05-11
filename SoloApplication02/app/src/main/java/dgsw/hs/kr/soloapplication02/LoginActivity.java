6921bec8591a0e2f093b43880ecfc4c373273744
MinwookJo <alsdnr0537@naver.com>
2018-05-11 21:07:14 +0900

    20180511
    facebookLogin

--- SoloApplication02/app/src/main/java/dgsw/hs/kr/soloapplication02/LoginActivity.java
+++ SoloApplication02/app/src/main/java/dgsw/hs/kr/soloapplication02/LoginActivity.java
@@ -1,10 +1,15 @@
 package dgsw.hs.kr.soloapplication02;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.facebook.AccessToken;
 import com.facebook.AccessTokenTracker;
@@ -17,26 +22,31 @@ import com.facebook.GraphResponse;
 import com.facebook.Profile;
 import com.facebook.ProfileTracker;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.internal.ImageRequest;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
-
     private CallbackManager callbackManager;
-    AccessTokenTracker accessTokenTracker;
-    AccessToken accessToken;
-    ProfileTracker profileTracker;
+    String profilePicUrl="Nope";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(this.getApplicationContext());
         setContentView(R.layout.activity_login);
+
+        final ImageView imageView = (ImageView) findViewById(R.id.profile);
 
         callbackManager = CallbackManager.Factory.create();
 
@@ -47,11 +57,22 @@ public class LoginActivity extends AppCompatActivity {
             public void onSuccess(LoginResult loginResult) {
                 GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
                     @Override
-                    public void onCompleted(JSONObject object, GraphResponse response) {
+                    public void onCompleted(final JSONObject object, GraphResponse response) {
                         Log.v("result",object.toString());
+                        //프로필 가져오기
+                        ProfileTracker profileTracker = new ProfileTracker() {
+                            @Override
+                            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                                if(object!=null) {
+                                    Uri imageUri = currentProfile.getProfilePictureUri(400, 400);
+                                    profilePicUrl = imageUri.toString();
+                                    Log.d("token:", currentProfile + "AND" + imageUri);
+                                    Picasso.get().load(profilePicUrl).into(imageView);
+                                }
+                            }
+                        };
                     }
                 });
-
                 Bundle parameters = new Bundle();
                 parameters.putString("fields", "id,name,email,gender,birthday");
                 graphRequest.setParameters(parameters);
@@ -68,6 +89,7 @@ public class LoginActivity extends AppCompatActivity {
                 Log.e("LoginErr",error.toString());
             }
         });
+
     }
 
     @Override
